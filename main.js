abc03dcd0b60990359f90a32fbfd67e0c3364a07
kauanpereiramoraes <106698831+kauanpereiramoraes@users.noreply.github.com>
2024-03-13 14:26:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -16,6 +16,5 @@ for(let i=0;i <botoes.length;i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+contadores[0].textContent = calculaTempo9tempoObjetivo1);
